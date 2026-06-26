688891487cf51c8652e69bd0a4a6e293f603df3e
nursiepotter-hub <nursiepotter@gmail.com>
2026-06-26 13:53:11 +0200

    Ajouter bouton WhatsApp flottant

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from '@/context/CartContext'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import WhatsAppButton from '@/components/WhatsAppButton'
 import Home from '@/pages/Home'
 import Products from '@/pages/Products'
 import Cart from '@/pages/Cart'
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
         <Footer />
+        <WhatsAppButton />
       </CartProvider>
     </BrowserRouter>
   )
